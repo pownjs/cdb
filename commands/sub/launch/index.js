35ded61ff49baa1150b36cacf993d3bdb39e96b5
Petko D. Petkov <pdp@gnucitizen.org>
2019-05-29 18:37:51 +0000

    Added additional launch options.

--- commands/sub/launch/index.js
+++ commands/sub/launch/index.js
@@ -10,16 +10,49 @@ exports.yargs = {
             alias: 'p',
             default: 9222,
         })
+
+        yargs.option('xss-auditor', {
+            describe: 'Turn on/off XSS auditor',
+            type: 'boolean',
+            alias: 'x',
+            default: true
+        })
+
+        yargs.option('certificate-errors', {
+            describe: 'Turn on/off certificate errors',
+            type: 'boolean',
+            alias: 'c',
+            default: true
+        })
+
+        yargs.option('pentest', {
+            describe: 'Start with prefered settings for pentesting',
+            type: 'boolean',
+            alias: 't',
+            default: false
+        })
     },
 
     handler: async(argv) => {
-        const { port: PORT } = argv
+        const { port: PORT, xssAuditor, certificateErrors, pentest } = argv
+
+        const commonArgs = ['--no-first-run', '--remote-debugging-port=$PORT']
+
+        if (!xssAuditor || pentest) {
+            commonArgs.push('--disable-xss-auditor')
+        }
+
+        if (!certificateErrors || pentest) {
+            commonArgs.push('--ignore-certificate-errors')
+        }
 
         const { spawn } = require('child_process')
 
         switch (process.platform) {
             case 'darwin':
-                spawn('/bin/bash', ['-c', '"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) --no-first-run --remote-debugging-port=$PORT'], { detached: true, stdio: 'ignore', env: { PORT } }).unref()
+                const args = ['-c', '"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX)' + commonArgs.join(' ')]
+
+                spawn('/bin/bash', args, { detached: true, stdio: 'ignore', env: { PORT } }).unref()
 
                 break
 
