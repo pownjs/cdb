5cd936710e335e7174aa3a9b554ccc974c373fd0
Petko D. Petkov <pdp@gnucitizen.org>
2019-05-29 13:12:36 +0000

    Helpful debug messages.

--- commands/sub/launch/index.js
+++ commands/sub/launch/index.js
@@ -19,14 +19,23 @@ exports.yargs = {
         switch (process.platform) {
             case 'darwin':
                 spawn('/bin/bash', ['-c', '"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) --no-first-run --remote-debugging-port=$PORT'], { detached: true, stdio: 'ignore', env: { PORT } }).unref()
+
                 break
 
             case 'linux':
                 console.warn('linux is currently not supported')
+
+                console.info('launch chrome manually with the following command:')
+                console.info('chrome --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) --no-first-run --remote-debugging-port=$PORT')
+
                 break
 
             case 'win32':
                 console.warn('win32 is currently not supported')
+
+                console.info('launch chrome manually with the following command:')
+                console.info('chrome --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) --no-first-run --remote-debugging-port=$PORT')
+
                 break
         }
     }
