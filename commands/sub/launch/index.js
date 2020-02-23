deb97850b79a82132a5e0538504650b1b35df2f0
Petko D. Petkov <pdp@gnucitizen.org>
2020-02-23 11:49:01 +0000

    Externalise launch function.

--- commands/sub/launch/index.js
+++ commands/sub/launch/index.js
@@ -41,58 +41,10 @@ exports.yargs = {
     },
 
     handler: async(argv) => {
-        const { port: PORT, xssAuditor, certificateErrors, proxy, pentest, url } = argv
+        const { launch } = require('../../../lib/launch')
 
-        const commonArgs = ['--no-first-run', `--remote-debugging-port=${PORT}`]
+        const { port, xssAuditor, certificateErrors, proxy, pentest, url } = argv || {}
 
-        if (!xssAuditor || pentest) {
-            commonArgs.push('--disable-xss-auditor')
-        }
-
-        if (!certificateErrors || pentest) {
-            commonArgs.push('--ignore-certificate-errors')
-        }
-
-        if (proxy) {
-            if (proxy === 'auto') {
-                if (pentest) {
-                    commonArgs.push('--proxy-server=localhost:8080')
-                }
-            }
-            else {
-                commonArgs.push(`--proxy-server=${proxy}`)
-            }
-        }
-
-        if (url) {
-            commonArgs.push(url)
-        }
-
-        const { spawn } = require('child_process')
-
-        switch (process.platform) {
-            case 'darwin':
-                const args = ['-c', `"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) ${commonArgs.join(' ')}`]
-
-                spawn('/bin/bash', args, { detached: true, stdio: 'ignore', env: { PORT } }).unref()
-
-                break
-
-            case 'linux':
-                console.warn('linux is currently not supported')
-
-                console.info('launch chrome manually with the following command:')
-                console.info(`chrome --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) ${commonArgs.join(' ')}`)
-
-                break
-
-            case 'win32':
-                console.warn('win32 is currently not supported')
-
-                console.info('launch chrome manually with the following command:')
-                console.info(`chrome --user-data-dir=$(mktemp -d /tmp/google-chome.XXXXXXX) ${commonArgs.join(' ')}`)
-
-                break
-        }
+        await launch({ port, xssAuditor, certificateErrors, proxy, pentest, url })
     }
 }
